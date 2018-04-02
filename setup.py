44313d61d778300709feb8415a4c63d760eebf1b
Tanay PrabhuDesai <tanayseven@gmail.com>
2018-04-03 00:04:46 +0530

    Moved creation of app to setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,9 @@ setup(
     packages=['rest_test'],
     include_package_data=True,
     install_requires=[
-        'flask',
-        'requests',
-        'pytest',
+        'flask==0.12.2',
+        'requests==2.18.4',
+        'pytest==3.4.2',
+        'Flask-Testing==0.7.1',
     ],
 )
