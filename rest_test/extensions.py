f33ebf68df0b5bfe1c3291e9ab34143690222f59
Tanay PrabhuDesai <tanayseven@gmail.com>
2018-04-07 23:40:23 +0530

    Add types

--- rest_test/extensions.py
+++ rest_test/extensions.py
@@ -4,10 +4,10 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app: Flask = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 db: SQLAlchemy = SQLAlchemy(app)
-migrate = Migrate(app, db)
+migrate: Migrate = Migrate(app, db)
