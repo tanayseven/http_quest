3f1e3d28cb769ea5ca3e8770c44ce764c2b56b7e
Tanay PrabhuDesai <tanayseven@gmail.com>
2018-05-06 19:13:30 +0530

    Move the config to a separate config file

--- rest_test/app.py
+++ rest_test/app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from flask_injector import FlaskInjector
 
+from rest_test.config import apply_dev_config
 from rest_test.di import configure
 from rest_test.extensions import db, jwt, migrate, mail
 from rest_test.product.view import products_view
@@ -11,20 +12,8 @@ from rest_test.view import root_view
 
 app: Flask = Flask(__name__.split('.')[0])
 
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-
-# Config for SQL-Alchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
-
-# Config for Flask-JWT
-app.config['JWT_AUTH_USERNAME_KEY'] = 'email'
-app.config['JWT_AUTH_PASSWORD_KEY'] = 'password'
-app.config['JWT_AUTH_URL_RULE'] = '/user/login'
-
-# Config for Flask-Mail
-app.config['MAIL_SERVER'] = 'mailcatcher'
-app.config['MAIL_PORT'] = '1025'
-app.config['MAIL_DEFAULT_SENDER'] = 'noreply@resttest.com'
+if os.environ.get('APP_ENVIRONMENT').lower() == 'dev':
+    apply_dev_config(app)
 
 # Perform migrations on the database
 db.init_app(app)
---
+++ rest_test/config.py
@@ -0,0 +1,14 @@
+import os
+
+
+def apply_dev_config(app):
+    app.config.update(
+        SECRET_KEY=os.environ.get('SECRET_KEY'),
+        SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URI'),
+        JWT_AUTH_USERNAME_KEY='email',
+        JWT_AUTH_PASSWORD_KEY='password',
+        JWT_AUTH_URL_RULE='/user/login',
+        MAIL_SERVER='mailcatcher',
+        MAIL_PORT='1025',
+        MAIL_DEFAULT_SENDER='noreply@resttest.com',
+    )
