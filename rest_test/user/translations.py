a5ffe921e160a2bd7fcc5b855d35aedd07b4051a
Tanay PrabhuDesai <tanayseven@gmail.com>
2018-05-13 09:43:08 +0530

    Move all strings from view to translations

--- rest_test/user/translations.py
+++ rest_test/user/translations.py
@@ -2,6 +2,11 @@ from rest_test.utilities import get_translation_for
 
 en_IN = {
     'user_not_found': 'User could not be found',
+    'user_created': 'The user has been created and the password information is sent to them',
+    'user_already_exists': 'The user already exists',
+    'login_instruction': 'To login, please POST `login_format` on /login',
+    'your_email': '<your_email>',
+    'your_password': '<your_password>',
     'password_reset_instructions_sent_to_email': 'Password reset instructions successfully sent to your email address',
     'password_reset_mail_subject': 'Password Reset Instructions',
     'password_reset_mail_message': 'Use this token to reset your password, send a json_format to /user/new_password/<reset_token>',
@@ -9,6 +14,11 @@ en_IN = {
 
 hi_IN = {
     'user_not_found': 'यूजर नहीं मिल सका।',
+    'user_created': 'यूजर बनाया गया है और पासवर्ड जानकारी उन्हें भेजी गयी है।',
+    'user_already_exists': 'यूजर पहले से मौजूद है।',
+    'login_instruction': 'लॉगिन करने के लिए, कृपया `login_format` को POST /login पर भेजें।',
+    'your_email': '<आपका_ईमेल>',
+    'your_password': '<आपका पासवर्ड>',
     'password_reset_instructions_sent_to_email': 'पासवर्ड रीसेट निर्देश सफलतापूर्वक आपके ईमेल पते पर भेजे गए है।',
     'password_reset_mail_subject': 'पासवर्ड रीसेट सूचना',
     'password_reset_mail_message': 'अपना पासवर्ड रीसेट करने के लिए इस टोकन(token) का प्रयोग करें, json_format को /user/new_password/<reset_token> पर आपका नया पासवर्ड(new_password) भेजें।',
--- rest_test/user/view.py
+++ rest_test/user/view.py
@@ -1,6 +1,7 @@
 from flask import jsonify, Blueprint, request
 from flask_jwt import jwt_required
 
+from rest_test.user.translations import get_text
 from rest_test.user.user import reset_password_for_user_having_email, create_user
 
 user_view = Blueprint('user', __name__)
@@ -9,8 +10,8 @@ user_view = Blueprint('user', __name__)
 @user_view.route('/user/login', methods=('GET',))
 def login_get():
     data = {
-        'message': 'to login, please POST `login_format` on /login',
-        'login_format': {'email': '<your_email>', 'password': '<your_password>'}
+        'message': get_text('login_instruction'),
+        'login_format': {'email': get_text('your_email'), 'password': get_text('your_password')}
     }
     return jsonify(data)
 
@@ -27,10 +28,10 @@ def password_reset():
 @jwt_required()
 def create_new():
     success_data = {
-        'message': 'the user has been created and the password information is sent to them',
+        'message': get_text('user_created'),
     }
     success = create_user(request.json.get('email'), request.json.get('password'))
     if success:
         return jsonify(success_data)
     else:
-        return jsonify({'message': 'the user already exists'}), 400
+        return jsonify({'message': get_text('user_already_exists')}), 400
