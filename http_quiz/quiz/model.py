0f4f7f38be1191c18ba2d68f4857424b6b494db0
Tanay PrabhuDesai <tanayseven@gmail.com>
2018-05-15 22:43:26 +0530

    Add new table Sequential Quiz

--- http_quiz/quiz/model.py
+++ http_quiz/quiz/model.py
@@ -1,5 +1,7 @@
 from enum import Enum
 
+from sqlalchemy.dialects.postgresql import JSONB
+
 from http_quiz.extensions import db
 
 
@@ -27,3 +29,12 @@ class Candidate(db.Model):
     quiz_type = db.Column(db.String(255), nullable=False)
     status = db.Column(db.String(64))
     created_by = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+class SequentialQuiz(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    candidate_id = db.Column(db.Integer, db.ForeignKey('candidate.id'))
+    input = db.Column(JSONB)
+    output = db.Column(JSONB)
+    attempts = db.Column(db.Integer, default=1)
+    status = db.Column(db.String(64))
