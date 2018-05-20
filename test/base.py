d114e196ab793e5a20f0ca3da1a65497dcba71bd
Tanay PrabhuDesai <tanayseven@gmail.com>
2018-05-20 21:39:08 +0530

    Refactor test setup

--- test/base.py
+++ test/base.py
@@ -65,13 +65,17 @@ class ApiTestBase(DatabaseTest):
         with mail.record_messages() as outbox:
             yield outbox
 
+    @staticmethod
+    def mask_bcrypt_to_use_plain_text():
+        if os.environ.get('FAST_TESTS'):
+            bcrypt.generate_password_hash = lambda x: x.encode()
+            bcrypt.check_password_hash = lambda x, y: x == y
+
     @pytest.fixture(autouse=True)
     def api_setup(self, mail_outbox):
         self.app_test = self.create_app()
         self.mail_outbox = mail_outbox
-        if os.environ.get('FAST_TESTS'):
-            bcrypt.generate_password_hash = lambda x: x.encode()
-            bcrypt.check_password_hash = lambda x, y: x == y
+        self.mask_bcrypt_to_use_plain_text()
         self.app_test.post()
         self.app_test.post_json = MethodType(_post_json, self.app_test)
 
