c81d7efec6357c60e9e61d737fd4287dd445f287
Tanay PrabhuDesai <tanayseven@gmail.com>
2018-07-11 16:28:37 +0530

    Add a stepdef that adds runs browser

--- features/example.feature
+++ features/example.feature
@@ -3,4 +3,5 @@ Feature: Show off behave
   Scenario: Run a simple test
     Given we have behave installed
       When we implement 5 tests
+      When we start selenium webbrowser
       Then behave will test them for us!
--- features/steps/example_steps.py
+++ features/steps/example_steps.py
@@ -1,4 +1,37 @@
 from behave import given, when, then
+import os
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class WebDriver:
+    DOWNLOAD_DIR = '/tmp'
+
+    def __init__(self):
+        headless=bool(os.environ.get('HEADLESS'))
+        self.options = webdriver.ChromeOptions()
+        self.options.add_argument('--disable-extensions')
+        if headless:
+            self.options.add_argument('--headless')
+            self.options.add_argument('--disable-gpu')
+            self.options.add_argument('--no-sandbox')
+        self.options.add_experimental_option(
+            'prefs', {
+                'download.default_directory': self.DOWNLOAD_DIR,
+                'download.prompt_for_download': False,
+                'download.directory_upgrade': True,
+                'safebrowsing.enabled': True
+            }
+        )
+        self.driver = webdriver.Chrome(chrome_options=self.options)
+        self.driver.implicitly_wait(10)
+
+    def close(self):
+        self.driver.quit()
+
+    def login(self):
+        self.driver.get('http://localhost')
+
+
 
 @given('we have behave installed')
 def step_impl(context):
@@ -8,6 +41,12 @@ def step_impl(context):
 def step_impl(context, number):
     assert number > 1 or number == 0
     context.tests_count = number
+
+@when('we start selenium webbrowser')
+def step_impl(context):
+    context.webbrowser = WebDriver()
+    context.webbrowser.login()
+    context.webbrowser.close()
 
 @then('behave will test them for us!')
 def step_impl(context):
