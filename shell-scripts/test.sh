6a55113c7de39eb1ef6aae17289235f9ad302f5c
Tanay PrabhuDesai <tanayseven@gmail.com>
2019-12-29 00:56:50 +0000

    Change the way of downloading and uploading coverage from s3

--- shell-scripts/test.sh
+++ shell-scripts/test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 docker-compose run integration-tests poetry run pytest --cov http_quest --cov test/ || exit 1
-aws s3 sync .coverage s3://tanayseven-http-quest-backend-build/$TRAVIS_BUILD_NUMBER/coverage_files/.coverage
+aws s3 sync . s3://tanayseven-http-quest-backend-build/$TRAVIS_BUILD_NUMBER/coverage_files/ --exclude="*" --include ".coverage"
