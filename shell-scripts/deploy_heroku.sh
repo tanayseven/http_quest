d9a6ca49715bc1f6c0d5990892eca3cbb9c454a1
Tanay PrabhuDesai <tanayseven@gmail.com>
2020-01-01 00:20:13 +0000

    Add health check run after deploy

--- shell-scripts/deploy_heroku.sh
+++ shell-scripts/deploy_heroku.sh
@@ -2,3 +2,4 @@
 heroku container:login || exit 1
 heroku container:push web --app http-quest-backend || exit 1
 heroku container:release web --app http-quest-backend || exit 1
+curl --fail https://http-quest-backend.herokuapp.com/health || exit 1
